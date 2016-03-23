47fef48e462b017b4ed0a8dc65cb890a3ca2f78f
Christian Reinbacher <reinbacher@icg.tugraz.at>
2016-03-23 13:35:35 +0100

    made matlab code compile the c++ part with gcc

--- CPP_module/compileMexModule.m
+++ CPP_module/compileMexModule.m
@@ -13,7 +13,10 @@ end
 
 
 %%
+if(ispc)
+    eval(['mex -largeArrayDims -v COMPFLAGS="/openmp $COMPFLAGS" CXXFLAGS="\$CXXFLAGS -fopenmp /openmp" CFLAGS="\$CFLAGS -fopenmp /openmp" LDFLAGS="\$LDFLAGS -fopenmp /openmp" LINKFALGS="$LINKFALGS /openmp" -output ../flexBoxCPP functions.cpp ']);
+else
+    eval(['mex -largeArrayDims -v COMPFLAGS=" $COMPFLAGS" CXXFLAGS="\$CXXFLAGS -fopenmp " CFLAGS="\$CFLAGS -fopenmp " LDFLAGS="\$LDFLAGS -fopenmp " LINKFALGS="$LINKFALGS " -output ../flexBoxCPP functions.cpp ']);
 
-eval(['mex -largeArrayDims -v COMPFLAGS="/openmp $COMPFLAGS" CXXFLAGS="\$CXXFLAGS -fopenmp /openmp" CFLAGS="\$CFLAGS -fopenmp /openmp" LDFLAGS="\$LDFLAGS -fopenmp /openmp" LINKFALGS="$LINKFALGS /openmp" -output ../flexBoxCPP functions.cpp ']);
-
+end
 cd '..'
