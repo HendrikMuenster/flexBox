350bf1f7fb8ac653647f711ba11a8a8eb3228c55
hendrikMuenster <hendrik.dirks@wwu.de>
2016-10-27 16:28:59 +0200

    tol parameter is now transfered to the C++ module

--- CPP_module/compileCudaModule.m
+++ CPP_module/compileCudaModule.m
@@ -19,6 +19,9 @@ end
 if ismac || isunix
     CUDA_INCLUDE='/usr/local/cuda/include';
     CUDA_LIB64='/usr/local/cuda/lib64';
+    GCC_LINE = '';
+%you may have to change the C++ compiler by adding the following line
+%GCC_LINE = 'GCC="/usr/bin/gcc-4.7"';
 end
 
 %%
@@ -42,7 +45,7 @@ dlmwrite('cudaCompileResult.txt',cmdout,'delimiter','')
 if ismac
     mex('-largeArrayDims CXXFLAGS="$CXXFLAGS -fopenmp -std=c++11" LDFLAGS="$LDFLAGS -fopenmp" -I"',CUDA_INCLUDE,'" -L"',CUDA_LIB64,'" -L/lib -lcusparse -lcudart -lcufft -lrt -output ../flexBoxCPP -lstdc++ "functionsCuda.cpp"');
 elseif isunix
-    eval(['mex -largeArrayDims CXXFLAGS="$CXXFLAGS -fopenmp -std=c++11" LDFLAGS="$LDFLAGS -fopenmp" -I"',CUDA_INCLUDE,'" -L"',CUDA_LIB64,'" -L/lib -lcusparse -lcudart -lcufft -lrt -output ../flexBoxCPP -lstdc++ "functionsCuda.cpp"']);
+    eval(['mex -v ',GCC_LINE,' -largeArrayDims CXXFLAGS="$CXXFLAGS -fopenmp -fPIC -std=c++11" LDFLAGS="$LDFLAGS -fopenmp" -I"',CUDA_INCLUDE,'" -L"',CUDA_LIB64,'" -L/lib -lcusparse -lcudart -lcufft -lrt -output ../flexBoxCPP -lstdc++ "functionsCuda.cpp"']);
 elseif ispc
     eval(['mex -largeArrayDims CXXFLAGS="$CXXFLAGS -fopenmp -std=c++11" LDFLAGS="$LDFLAGS -fopenmp" -I"',CUDA_INCLUDE,'" -L"',CUDA_LIB64,'" -L/lib -lcusparse -lcudart -lcufft -output ../flexBoxCPP "functionsCuda.cpp"']);
 end 
