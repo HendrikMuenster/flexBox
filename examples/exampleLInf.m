e7e9ba1c3817ef0d566d4963cd6c6bbea3897876
HendrikMuenster <hendrik.dirks@wwu.de>
2017-07-15 13:12:56 +0200

    small bugfix in MATLAB calculation and output improvements

--- examples/exampleLInf.m
+++ examples/exampleLInf.m
@@ -3,15 +3,19 @@ clear all;close all;clc;
 %% read data
 addpath(genpath('..'));
 
-weightL2 = 3;
-weightLInf = 25.5;
+weightL2 = 0.1;
+weightLInf = 2.5;
 dataPart = [10, 12];
 [X,Y] = meshgrid(-20:0.5:20,-20:0.5:20);
 fx = @(X,Y) weightL2/2*((X - dataPart(1)).^2 + (Y - dataPart(2)).^2) + weightLInf*(max(abs(X),abs(Y)));
 Z = fx(X,Y);
-surf(X,Y,Z);
-minX = fminunc(@(x) fx(x(1),x(2)), [0,0]);
-disp(["MATLAB: ", minX]);
+surf(X,Y,Z);rotate3d on;
+
+[minX,fval] = fminsearch(@(x) fx(x(1),x(2)), [1,1]);
+disp('Calculating minimal function value using MATLAB')
+disp(['argmin: ', num2str(minX)]);
+disp(['min: ', num2str(fval)]);
+
 %%
 main = flexBox;
 main.params.tryCPP = 0; %change, if C++ module is compiled
@@ -29,9 +33,7 @@ tic;main.runAlgorithm;toc;
 
 %% get result
 result = main.getPrimal(numberT);
-disp(result);
-fx(minX(1),minX(2))
-fx(result(1),result(2))
-%% show result
-%figure(2);
-%plot(result);
+
+disp('Calculating minimal function value using FlexBox')
+disp(['argmin: ', num2str([result(1),result(2)])]);
+disp(['min: ', num2str(fx(result(1),result(2)))]);
