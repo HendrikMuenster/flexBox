eb5ce7400254f2070875732c3fd132d4febc5707
Lars Haalck <lars.haalck@gmail.com>
2017-07-16 14:29:54 +0200

    fixed formatting in exampleLInf

--- examples/exampleLInf.m
+++ examples/exampleLInf.m
@@ -24,5 +24,5 @@ tic;main.runAlgorithm;toc;
 minXFlex = main.getPrimal(numberT);
 
 %% print result
-disp(['FlexBox min: ', minXFlex]);
-disp(['with value of: ', func(minXFlex)]);
+disp(['FlexBox min: ', num2str(minXFlex)]);
+disp(['with value of: ', num2str(func(minXFlex))]);
