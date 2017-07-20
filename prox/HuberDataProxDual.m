0fd110e0e9f68fc7932bbe15acfbbb85dadac057
jonas.geiping <jonas.geiping@googlemail.com>
2017-07-20 10:25:45 +0200

    Typo in HuberDataProx

--- prox/HuberDataProxDual.m
+++ prox/HuberDataProxDual.m
@@ -7,12 +7,12 @@ classdef HuberDataProxDual < basicProx
         function obj = HuberDataProxDual()
         end
 
-        function applyProx(obj,main,dualNumber,~)
+        function applyProx(obj,main,dualNumbers,~)
             %calc norm
             norm = 0;
             for i=1:obj.numVars
                 
-				main.yTilde{dualNumbers(i)} = (main.yTilde{dualNumbers(i)} - main.params.sigma{dualNumber(i)}.*obj.f{i}(:)) .* obj.factor ./ (obj.factor + obj.epsi.*main.params.sigma{dualNumbers(i)});
+				main.yTilde{dualNumbers(i)} = (main.yTilde{dualNumbers(i)} - main.params.sigma{dualNumbers(i)}.*obj.f{i}(:)) .* obj.factor ./ (obj.factor + obj.epsi.*main.params.sigma{dualNumbers(i)});
 				
 				norm = max(1,abs(main.yTilde{dualNumbers(i)}) / obj.factor);
 				
